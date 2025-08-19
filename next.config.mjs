a42da073a065cedc8cbe1e46f00867a33efb71ef
Sukna <genomicvoid@gmail.com>
2025-08-20 06:52:20 +0700

    Trigger workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/portfolio1-gilang.github.io",
   trailingSlash: true,
   images: {
     unoptimized: true,
