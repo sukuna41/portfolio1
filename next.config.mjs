f4e84e8925d15aba08b100aa4ab320a05d5202a1
Sukna <genomicvoid@gmail.com>
2025-08-20 08:49:44 +0700

    Asw

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,17 @@
 // next.config.mjs
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
-  output: "export", // ◀── INI YANG PENTING!
+  output: "export",
   trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
+  images: { unoptimized: true },
+  ...(isProd
+    ? {
+        basePath: "/portfolio1-gilang.github.io",
+        assetPrefix: "/portfolio1-gilang.github.io/",
+      }
+    : {}),
 };
 
 export default nextConfig;
