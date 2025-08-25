c1053aff08966de72d9095e6778b5b326f1e2b51
Sukna <genomicvoid@gmail.com>
2025-08-25 15:52:17 +0700

    fix image at certificate

--- app/resume/data/dataCertificates.jsx
+++ app/resume/data/dataCertificates.jsx
@@ -2,21 +2,21 @@ export const certificates = [
   {
     id: 1,
     name: "Belajar Sketch Up",
-    image_url: "/images/certificates/desain3d.jpg",
+    image_url: "/portfolio1/images/certificates/desain3d.jpg",
   },
   {
     id: 2,
     name: "Microsoft Office Pegawai Kantoran",
-    image_url: "/images/certificates/microsoftoffice.jpg",
+    image_url: "/portfolio1/images/certificates/microsoftoffice.jpg",
   },
   {
     id: 3,
     name: "Software Engineering Pemula",
-    image_url: "/images/certificates/softwareengineer.jpg",
+    image_url: "/portfolio1/images/certificates/softwareengineer.jpg",
   },
   {
     id: 4,
     name: "HTML 5 Application Development",
-    image_url: "/images/certificates/HTML5.jpg",
+    image_url: "/portfolio1/images/certificates/HTML5.jpg",
   },
 ];
