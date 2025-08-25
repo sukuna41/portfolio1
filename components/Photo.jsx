6c8bdc9a1a1d97de8613353cf037279f315e92bf
Sukna <genomicvoid@gmail.com>
2025-08-25 15:04:00 +0700

    using  Image from Next Image

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -1,8 +1,5 @@
-"use client";
-import { motion } from "framer-motion";
-import Image from "next/image";
-
 const Photo = () => {
+  console.log("Rendering Photo component");
   return (
     <motion.div
       initial={{ opacity: 0, scale: 0.5 }}
@@ -10,20 +7,23 @@ const Photo = () => {
         opacity: 1,
         scale: 1,
         transition: {
-          delay: 0.5, // Delay untuk sinkronisasi
+          delay: 0.5,
           duration: 1,
-          ease: [0.22, 1, 0.36, 1], // Kurva easing premium
+          ease: [0.22, 1, 0.36, 1],
         },
       }}
       className="relative w-full max-w-[480px] h-[420px] xl:max-w-full xl:h-[520px] xl:mx-0 mt-6 xl:mt-6 border-2 border-accent"
     >
-      <img
+      <Image
         src="/image/profile.png"
         alt="Gilang Ramadhan"
-        className="object-cover w-full h-full"
+        fill
+        className="object-cover"
+        quality={100}
+        style={{
+          objectPosition: "center",
+        }}
       />
     </motion.div>
   );
 };
-
-export default Photo;
