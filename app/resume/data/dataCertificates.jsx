7bb13ff5bd1c9601880f0933663f73bf63729e19
Sukna <genomicvoid@gmail.com>
2025-10-05 00:51:56 +0700

    add SKL

--- app/resume/data/dataCertificates.jsx
+++ app/resume/data/dataCertificates.jsx
@@ -11,16 +11,21 @@ export const certificates = [
   },
   {
     id: 3,
+    name: "Keterangan Lulus",
+    image_url: "/portfolio1/images/certificates/SKL.png",
+  },
+  {
+    id: 4,
     name: "Belajar Sketch Up",
     image_url: "/portfolio1/images/certificates/desain3d.jpg",
   },
   {
-    id: 4,
+    id: 5,
     name: "Microsoft Office Pegawai Kantoran",
     image_url: "/portfolio1/images/certificates/microsoftoffice.jpg",
   },
   {
-    id: 5,
+    id: 6,
     name: "Software Engineering Pemula",
     image_url: "/portfolio1/images/certificates/softwareengineer.jpg",
   },
